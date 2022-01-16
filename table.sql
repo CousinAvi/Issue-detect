698d9b943e7979c8465b2ebc5e57f083d63b1e5c
CousinAvi <Washington_Capitals_1@mail.ru>
2022-01-16 18:29:41 +1000

    Добавлен SQL запрос поиска для названий организаций

--- table.sql
+++ table.sql
@@ -3,3 +3,6 @@ create table domain_fin (orgName VARCHAR(200), regNumber INT, domen VARCHAR(200)
 
 # Таблица для статистики по пингу ресурсов ФО
 create table ping_status (orgName VARCHAR(200), domen VARCHAR(200), ping int1)
+
+# Запрос для сервиса детекта сообщений пользователей
+select * from domain_fin where orgName like '%Сбербанк%'
